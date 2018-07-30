4aee33ec29815f75ff820055aff5121858d92f26
AdvaitParulekar <adutheparulekar@tamu.edu>
2018-07-30 16:48:57 -0500

    asd

--- elipopdianaelimin8/driver.py
+++ elipopdianaelimin8/driver.py
@@ -30,6 +30,9 @@ class MyGame(arcade.Window):
         self.sprite_list = arcade.SpriteList()
         self.static_lines = []
 
+        self.shape_being_dragged = None
+        self.last_mouse_position = 0, 0
+
         self.draw_time = 0
         self.processing_time = 0
 
@@ -37,7 +40,7 @@ class MyGame(arcade.Window):
         floor_height = 100
         floor_body = pymunk.Body(body_type=pymunk.Body.STATIC)
         floor_shape = pymunk.Segment(floor_body, [0, floor_height], [SCREEN_WIDTH, floor_height], 0.0)
-        floor_shape.friction = 10
+        floor_shape.friction = 0.7
         self.space.add(floor_shape)
         self.static_lines.append(floor_shape)
 
@@ -53,32 +56,56 @@ class MyGame(arcade.Window):
         #torso
         torso_moment = pymunk.moment_for_segment(torso_mass, (0, 0), (0, torso_length), 3)
         torso_body = pymunk.Body(torso_mass, torso_moment)
-        torso_body.position = pymunk.Vec2d(300, 500)
+        torso_body.position = pymunk.Vec2d(300, 200)
         torso_shape = pymunk.Poly.create_box(torso_body, (10, 100))
         self.space.add(torso_body, torso_shape)
-        torso_sprite = PhysicsSprite(torso_shape, "torso.png", 1)
+        torso_sprite = PhysicsSprite(torso_shape, "torso.png", 0.375)
         self.sprite_list.append(torso_sprite)
 
         #thigh
         thigh_moment = pymunk.moment_for_segment(thigh_mass, (0, 0), (0, thigh_length), 3)
 
         l_thigh_body = pymunk.Body(thigh_mass, thigh_moment)
-        l_thigh_body.position = pymunk.Vec2d(300, 300)
-        l_thigh_shape = pymunk.Poly.create_box(l_thigh_body, (10, 100))
+        l_thigh_body.position = pymunk.Vec2d(300, 125)
+        l_thigh_shape = pymunk.Poly.create_box(l_thigh_body, (5, 50))
+        l_thigh_shape.friction = 0.6
         self.space.add(l_thigh_body, l_thigh_shape)
-        l_thigh_sprite = PhysicsSprite(l_thigh_shape, "torso.png", 0.5)
+        l_thigh_sprite = PhysicsSprite(l_thigh_shape, "torso.png", 0.187)
         self.sprite_list.append(l_thigh_sprite)
 
         r_thigh_body = pymunk.Body(thigh_mass, thigh_moment)
-        r_thigh_body.position = pymunk.Vec2d(300, 300)
-        r_thigh_shape = pymunk.Poly.create_box(r_thigh_body, (10, 100))
+        r_thigh_body.position = pymunk.Vec2d(300, 125)
+        r_thigh_shape = pymunk.Poly.create_box(r_thigh_body, (5, 50))
+        r_thigh_shape.friction = 0.6
         self.space.add(r_thigh_body, r_thigh_shape)
-        r_thigh_sprite = PhysicsSprite(r_thigh_shape, "torso.png", 0.5)
+        r_thigh_sprite = PhysicsSprite(r_thigh_shape, "torso.png", 0.187)
         self.sprite_list.append(r_thigh_sprite)
 
-        #j = pymunk.PinJoint(l_thigh_body, torso_body)
-        #self.space.add(j)
+        j1 = pymunk.PinJoint(l_thigh_body, torso_body, (0, 26), (0, -52))
+        j2 = pymunk.PinJoint(r_thigh_body, torso_body, (0, 26), (0, -52))
+        self.space.add(j1, j2)
 
+    def on_mouse_press(self, x, y, button, modifiers):
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            self.last_mouse_position = x, y
+            # See if we clicked on anything
+            shape_list = self.space.point_query((x, y), 1, pymunk.ShapeFilter())
+
+            # If we did, remember what we clicked on
+            if len(shape_list) > 0:
+                self.shape_being_dragged = shape_list[0]
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        if self.shape_being_dragged is not None:
+            # If we are holding an object, move it with the mouse
+            self.last_mouse_position = x, y
+            self.shape_being_dragged.shape.body.position = self.last_mouse_position
+            self.shape_being_dragged.shape.body.velocity = dx * 20, dy * 20
+
+    def on_mouse_release(self, x, y, button, modifiers):
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            # Release the item we are holding (if any)
+            self.shape_being_dragged = None
 
     def on_draw(self):
         """
@@ -127,8 +154,10 @@ class MyGame(arcade.Window):
             sprite.center_x = sprite.pymunk_shape.body.position.x
             sprite.center_y = sprite.pymunk_shape.body.position.y
             sprite.angle = math.degrees(sprite.pymunk_shape.body.angle)
-            print(sprite.center_y)
 
+        if self.shape_being_dragged is not None:
+            self.shape_being_dragged.shape.body.position = self.last_mouse_position
+            self.shape_being_dragged.shape.body.velocity = 0, 0
         # Save the time it took to do this.
         self.processing_time = timeit.default_timer() - start_time
 
